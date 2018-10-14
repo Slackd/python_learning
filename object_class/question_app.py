582005b396c8a87df8f47c61a7552ad1d9ccfca0
Budhaditya Saha <budha.rocks@gmail.com>
2018-10-14 05:47:09 +0530

    Q/A Game

---
+++ object_class/question_app.py
@@ -0,0 +1,25 @@
+from Questions import Question
+
+question_prompts = [
+  "What color are Apples?\n (a) Red/Green\n (b) Blue\n (c) Orange\n\n",
+  "What color are Bananas?\n (a) Red\n (b) Green\n (c) Yellow\n\n",
+  "WWhat color are Strawberries?\n (a) Teal\n (b) Magenta\n (c) Red\n\n"
+]
+
+question = [
+  Question(question_prompts[0], "a"),
+  Question(question_prompts[1], "c"),
+  Question(question_prompts[2], "c"),
+]
+
+def run_test(questions):
+  score = 0
+  for question in questions:
+    answer = input(question.prompt)
+    if answer == question.answer:
+      score += 1
+  print("You got " + str(score) + "/" + str(len(questions)) + " Correct!")
+
+run_test (question)
+
+
